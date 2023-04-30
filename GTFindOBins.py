9e29b009ffb4bfd9ecb033fb2c5feac84d6d699d
nWwWm <marcin133mlynne@gmail.com>
2023-04-30 16:17:03 +0200

    Refactor GTFindOBins script for better clarity and maintainability
    This commit implements various changes to improve the GTFindOBins script. The changes made are as follows:
    
    Rename banner() to display_banner() to make the code more clear.
    Refactor display_banner() by hardcoding the banner and removing the pyfiglet import.
    Remove the get_version() function as it is not used.
    Add display_output() function to simplify printing to the console.
    Refactor read_file() to check the file extension before checking the mime type, improving efficiency.
    Refactor is_anchor_on_site() by adding exception handling and timeouts for requests, improving reliability.
    Refactor get_gtfobins_links() to simplify the function and return values instead of printing them.
    Refactor gtfobins_scraper() to simplify the function.
    Add options and exception handling to cli() to improve user experience.
    These changes make the code more maintainable and easier to understand.

--- GTFindOBins.py
+++ GTFindOBins.py
@@ -6,30 +6,36 @@ import json
 import click
 import magic
 import os
-import pkg_resources
-import pyfiglet
 import requests
 
 
-def get_version() -> str:
-    try:
-        return pkg_resources.require('gtfinderobins')[0].version
-    except:
-        return '-'
-
-def banner() -> None:
+def display_banner() -> None:
     """Displays a banner with the name of the script and some additional information."""
-    ascii_banner = pyfiglet.figlet_format('GTFindOBins', font='smkeyboard')
+    ascii_banner = '''
+  ________________         ______  ___  _        
+ / ___/_  __/ __(_)__  ___/ / __ \/ _ )(_)__  ___
+/ (_ / / / / _// / _ \/ _  / /_/ / _  / / _ \(_-<
+\___/ /_/ /_/ /_/_//_/\_,_/\____/____/_/_//_/___/
+                                                 
+    '''
     click.echo(ascii_banner)
     click.echo('# Probably Useless Script Coded By Marcin Chryczyk (@nWwWm)')
-    click.echo('# version: ' + get_version())
+    click.echo('# By the way, I didn\'t expect anyone to use this option ;)')
     click.echo('________________________________________________________')
     click.echo()
 
 
+def display_output(data: list[tuple[str, str]], all: bool = False) -> None:
+    for line in data:
+        if line[1] is None and all:
+            click.echo(f'[-] {line[0]} --> NOT FOUND (verbose mode)')
+        elif line[1]:
+            click.echo(f'[+] {line[0]} --> {line[1]}')
+
+
 def read_file(filename: str) -> list[str]:
     """Reads the data from the file.
-    
+
     Args:
         filename: Filename string. File supported formats: json, txt, csv
 
@@ -39,124 +45,130 @@ def read_file(filename: str) -> list[str]:
     mime = magic.from_file(filename, mime=True)
     ext = filename.split('.')[-1]
     data = []
+
+    if ext not in ['json', 'txt', 'csv']:
+        raise NotImplementedError('File extension not supported.')
+
     with open(filename, 'r') as f:
-            # check extension for csv file first because mime is the same as txt file.
-            if ext == 'csv':
-                reader = csv.reader(f)
-                data = list(reader)[0]
-            elif mime == 'text/plain' and ext == 'txt':
-                # Strip new line character
-                data = [line.strip() for line in f.readlines()]
-            elif mime == 'application/json' and ext == 'json':
-                data = json.load(f)
-            else:
-                raise NotImplementedError('File extension support not implemented.')
+        # check extension for csv file first because mime is the same as txt file.
+        if mime == 'text/plain' and ext == 'csv':
+            reader = csv.reader(f)
+            data = list(reader)[0]
+        elif mime == 'text/plain':
+            # Strip new line character
+            data = [line.strip() for line in f.readlines()]
+        elif mime == 'application/json' and ext == 'json':
+            data = json.load(f)
     return data
 
 
 def is_anchor_on_site(url: str, anchor: str) -> bool:
     """Checks if the anchor is on the page."""
-    result = requests.get(url)
-    if result.status_code != 200:
+
+    try:
+        result = requests.get(url, timeout=10)
+        result.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.Timeout):
         return False
-    doc = BeautifulSoup(result.text, 'html.parser')
+
+    try:
+        doc = BeautifulSoup(result.text, 'html.parser')
+    except:
+        return False
+
     return doc.find('a', {'href': anchor}) is not None
 
 
-def get_gtfobins_link(binary_name: str, function_name: str = None) -> None:
-    """Scrapes the GTFOBins website for the provided binary and function, and prints out the corresponding link.
-
-    Args:
-        binary_name: The name of the binary to search for on the GTFOBins website.
-        function_name: The name of the function to search for on the binary's page. Defaults to None.
-
-    Returns:
-        None: The function does not return anything, but prints out the link to the binary or function if found.
-
-    """
-    if binary_name is None:
-        click.echo('No binaries provided')
-        return None
+def get_gtfobins_link(binary_name: str, function_name: str | None = None) -> tuple[str, str | None]:
+    """Scrapes the GTFOBins website for the provided binary and function, and prints out the corresponding link."""
 
     url = f'https://gtfobins.github.io/gtfobins/{binary_name}/'
     result = requests.get(url)
 
     if result.status_code == 404:
-        click.echo(f"No link found for binary: {binary_name}")
-        return None
+        return (binary_name, None)
 
     doc = BeautifulSoup(result.text, 'html.parser')
-    
+
     if function_name is None:
         # if function name is not given, return link to the binary page
-        click.echo(url)
+        return (binary_name, url)
     else:
         # if function name is given, search for that function in the page
-        if is_anchor_on_site(url, f'/gtfobins/{binary_name}/#{function_name}'):
-            click.echo(url + f'#{function_name}')
-             
-        else:
-           click.echo(f"No Link found for function: {function_name} in binary: {binary_name}")
-        
-    
-def gtfobins_scrapper(bins: list[str] | None, funcs: list[str] | None) -> None:
-    """Scrapes the GTFOBins website for links to binaries or functions, or both.
-    
-    Args:
-        bins: A list of binary names to search for.
-        funcs: A list of function names to search for.
+        # On the site, anchor names are lowercase function names and instead of spaces they are -
+        function_anchor = function_name.lower().replace(' ', '-')
 
-    Returns:
-        None: The function does not return any values.
-    
-    """
+        if is_anchor_on_site(url, f'/gtfobins/{binary_name}/#{function_anchor}'):
+            return (f'{binary_name}+{function_name}', url + f'#{function_anchor}')
+        else:
+            return (f'{binary_name}+{function_name}', None)
+
+
+def gtfobins_scrapper(binary_name: list[str], function_name: list[str] | None = None) -> None:
+    """Scrapes the GTFOBins website for links to binaries, or binaries with functions."""
     data = []
-    if bins is not None and funcs is not None:
-        # Both parameters provided
-        with ThreadPoolExecutor() as e:
-            for bin in bins:
-                for func in funcs:
-                    e.submit(get_gtfobins_link, bin, func)
-    elif bins is not None:
-        # Only bins parameter provided
-        with ThreadPoolExecutor() as e:
-            for bin in bins:
-                e.submit(get_gtfobins_link, bin)
-    elif funcs is not None:
-        # Only func parameter provided
-        get_gtfobins_link(None, funcs)
-    else:
-        # No parameters provided
-        pass
+    features = []
+
+    with ThreadPoolExecutor() as e:
+        if function_name:
+            for bin in binary_name:
+                for func in function_name:
+                    features.append(e.submit(get_gtfobins_link, bin, func))
+        else:
+            for bin in binary_name:
+                features.append(e.submit(get_gtfobins_link, bin, None))
+
+    for feature in features:
+        data.append(feature.result())
+
+    return data
+
 
 @click.command()
-@click.option('-b', '--binary', help='Binary name or file name with binaries')
-@click.option('-f', '--function', help='Function name or file name with functions')   
-def cli(binary: str, function: str) -> None:
-    """GTFinderOBins is a script that searches the GTFOBins website for pages related to a given list of binaries and functions."""
-    
-    try:
-        if os.path.isfile(binary):
-            binaries = read_file(binary)
-        else:
-            binaries = binary.split(',')
-    except TypeError:
-        binaries = None
+@click.argument('binary')
+@click.option('-f', '--function', help='Name, filename or list (e.g. Sudo,SUID) of functions.')
+@click.option('--banner/--no-banner', '-B/ ', default=False, help='Display script banner.')
+@click.option('-v/ ', '--verbose/--no-verbose', default=False)
+def cli(binary: str, function: str, banner: bool, verbose: bool) -> None:
+    """GTFinderOBins is a script that searches the GTFOBins website for pages related to a given list of binaries and functions.
 
-    try:    
+    BINARY is the name, filename or list (e.g. nmap,base32) of binaries.
+
+    Supported file types: json | txt | csv
+    """
+
+    if banner:
+        display_banner()
+
+    if os.path.isfile(binary):
+        try:
+            binaries = read_file(binary)
+        except NotImplementedError:
+            click.echo(
+                'Error: Oops! That was not a valid file extension. For more info --help')
+            return
+    else:
+        binaries = binary.split(',')
+
+    try:
         if os.path.isfile(function):
             functions = read_file(function)
         else:
             functions = function.split(',')
     except TypeError:
         functions = None
-    
-    gtfobins_scrapper(binaries, functions)
+    except NotImplementedError:
+        click.echo(
+            'Error: Oops! That was not a valid file extension. For more info --help')
+        return
+
+    data = gtfobins_scrapper(binaries, functions)
+    display_output(data, verbose)
 
 
 def main() -> None:
-    banner()
     cli()
+
 
 if __name__ == "__main__":
     main()
